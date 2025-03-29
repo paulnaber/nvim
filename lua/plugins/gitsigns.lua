1c57167b093eb5a107ca6ba5be46bebcbb5d4e8e
Paul Naber <paul.naber@inside-m2m.de>
2025-03-30 00:31:56 +0100

    organize code, add readme, remove comments

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,6 +1,7 @@
 -- Adds git related signs to the gutter, as well as utilities for managing changes
 -- NOTE: gitsigns is already included in init.lua but contains only the base
 -- config. This will add also the recommended keymaps.
+-- See `:help gitsigns` to understand what the configuration keys do
 
 return {
   {
