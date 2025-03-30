1407f50dc30d11721547d6bb8c79c9d66d6d44ff
Paul Naber <paul.naber@inside-m2m.de>
2025-03-30 18:00:24 +0200

    add flash

--- init.lua
+++ init.lua
@@ -48,6 +48,7 @@ require('lazy').setup({
   -- require 'plugins.harpoon'
   require 'plugins.bufferline',
   require 'plugins.supermaven',
+  require 'plugins.flash',
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
