7684708e892d65eaa3b8822de34cfc63cabfdebb
Paul Naber <paul.naber@inside-m2m.de>
2025-03-30 20:49:45 +0200

    add prettier formatter

--- README.md
+++ README.md
@@ -6,8 +6,8 @@ https://github.com/nvim-lua/kickstart.nvim
 
 ### TODOs
 - harpoon?
+- remove tabs?
+- add splits
 - nx generators
 - show git lense all the time
-- angular lsp setup
-- prettier
 
--- lua/plugins/autoformat.lua
+++ lua/plugins/autoformat.lua
@@ -31,6 +31,15 @@ return { -- Autoformat
     end,
     formatters_by_ft = {
       lua = { 'stylua' },
+      python = { 'isort', 'black' }, -- Example: Uncomment if you format Python
+      javascript = { 'prettierd', 'prettier', stop_after_first = true },
+      typescript = { 'prettierd', 'prettier', stop_after_first = true },
+      json = { 'prettierd', 'prettier', stop_after_first = true },
+      yaml = { 'prettierd', 'prettier', stop_after_first = true },
+      html = { 'prettierd', 'prettier', stop_after_first = true },
+      css = { 'prettierd', 'prettier', stop_after_first = true },
+      scss = { 'prettierd', 'prettier', stop_after_first = true },
+      markdown = { 'prettierd', 'prettier', stop_after_first = true },
       -- Conform can also run multiple formatters sequentially
       -- python = { "isort", "black" },
       --
